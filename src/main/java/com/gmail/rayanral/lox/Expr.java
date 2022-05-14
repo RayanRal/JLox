0894111eabdfee1d9df047fa43b31d272d79604c
leonidchashnikov <rayanral@gmail.com>
2022-05-14 20:40:42 +0100

    Generate AST classes

--- src/main/java/com/gmail/rayanral/lox/Expr.java
+++ src/main/java/com/gmail/rayanral/lox/Expr.java
@@ -1,19 +1,40 @@
 package com.gmail.rayanral.lox;
 
+import java.util.List;
+
 abstract class Expr {
-
-    static class Binary extends Expr {
-        Binary(Expr left, Token operator, Expr right) {
-            this.left = left;
-            this.operator = operator;
-            this.right = right;
-        }
-
-        final Expr left;
-        final Token operator;
-        final Expr right;
+  static class Binary extends Expr {
+    final Expr left;
+    final Token operator;
+    final Expr right;
+    Binary(Expr left, Token operator, Expr right) {
+      this.left = left;
+      this.operator = operator;
+      this.right = right;
     }
 
+  }
+  static class Grouping extends Expr {
+    final Expr expression;
+    Grouping(Expr expression) {
+      this.expression = expression;
+    }
 
+  }
+  static class Literal extends Expr {
+    final Object value;
+    Literal(Object value) {
+      this.value = value;
+    }
 
+  }
+  static class Unary extends Expr {
+    final Token operator;
+    final Expr right;
+    Unary(Token operator, Expr right) {
+      this.operator = operator;
+      this.right = right;
+    }
+
+  }
 }
--- src/main/java/com/gmail/rayanral/tools/GenerateAST.java
+++ src/main/java/com/gmail/rayanral/tools/GenerateAST.java
@@ -1,6 +1,9 @@
 package com.gmail.rayanral.tools;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
 
 public class GenerateAST {
 
@@ -10,6 +13,63 @@ public class GenerateAST {
             System.exit(64);
         }
         String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Binary   : Expr left, Token operator, Expr right",
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(String outputDir, String baseClass, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseClass + ".java";
+
+        PrintWriter writer = new PrintWriter(path, "UTF-8");
+        writer.println("package com.gmail.rayanral.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseClass + " {");
+
+        for(String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseClass, className, fields);
+        }
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineType(
+            PrintWriter writer,
+            String baseName,
+            String className,
+            String fieldList
+    ) {
+
+        writer.println("  static class " + className + " extends " +
+                baseName + " {");
+        String[] fields = fieldList.split(", ");
+
+        // Declare fields.
+        for (String field : fields) {
+            writer.println("    final " + field + ";");
+        }
+
+        // Constructor.
+        writer.println("    " + className + "(" + fieldList + ") {");
+
+        // Store parameters in fields.
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("      this." + name + " = " + name + ";");
+        }
+        writer.println("    }");
+        writer.println();
+
+        writer.println("  }");
+
     }
 
 }
