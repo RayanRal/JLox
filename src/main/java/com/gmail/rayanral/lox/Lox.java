c403444f35704d33f0d67108f010ee852e9c48aa
leonidchashnikov <rayanral@gmail.com>
2022-05-28 23:08:54 +0100

    Parsing is still bad, but let's start handling errors

--- src/main/java/com/gmail/rayanral/lox/Lox.java
+++ src/main/java/com/gmail/rayanral/lox/Lox.java
@@ -57,6 +57,14 @@ public class Lox {
         report(line, "", message);
     }
 
+    static void error(Token token, String message) {
+        if(token.type == TokenType.EOF) {
+            report(token.line, "at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
     private static void report(int line, String where,
                                String message) {
         System.err.println(
