ff7e1caa2c629e3835f3a9c51553edb60c9fc34c
leonidchashnikov <rayanral@gmail.com>
2022-06-11 20:19:25 +0100

    a bit of changes

--- src/main/java/com/gmail/rayanral/lox/Interpreter.java
+++ src/main/java/com/gmail/rayanral/lox/Interpreter.java
@@ -122,12 +122,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
             arguments.add(evaluate(argument));
         }
 
-        if(!(callee instanceof LoxCallable)) {
+        if(!(callee instanceof LoxCallable function)) {
             throw new RuntimeError(expr.paren,
                     "Can only call functions and classes.");
         }
 
-        LoxCallable function = (LoxCallable) callee;
         if(arguments.size() != function.arity()) {
             throw new RuntimeError(expr.paren, "Expected " +
                     function.arity() + " arguments but got " +
